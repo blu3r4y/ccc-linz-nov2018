e979a40b8d9d4be2d7d27a729cf49a6f78d2199d
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-11-16 19:00:02 +0100

    level5 - unfinished

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from scipy.ndimage.measurements import label
 def main(input):
     grid = np.array(input["rows"])
 
-    mask, ncomponents = label(grid)
-
-    # is the floor in the upper left corner?
-    assert grid[0, 0] == 0
-
     buildings = []
-    for ncomponent in range(1, ncomponents + 1):
-        building = get_building(grid, mask, ncomponent)
 
-        is_hot = is_hotspot(*building)
-        if is_hot:
-            buildings.append(building)
+    heights = sorted(np.unique(grid))
+    for height in heights[1:]:
+        grid_on_height = np.where(grid == height, grid, 0)
 
-    # get center points
-    buildings = [get_center_point(*a) for a in buildings]
+        mask, ncomponents = label(grid_on_height)
+
+        # is the floor in the upper left corner?
+        assert grid_on_height[0, 0] == 0
+
+        for ncomponent in range(1, ncomponents + 1):
+            building = get_building(grid_on_height, mask, ncomponent)
+            hotspots = get_hotspots(grid_on_height, mask, building, ncomponent, input["s"])
+            buildings.extend(hotspots)
 
     # sort by row and by col
     buildings = sorted(buildings, key=lambda x: (x[0], x[1]))
@@ -31,6 +31,43 @@ def main(input):
     # [' '.join([' '.join(f) for f in e]) for e in buildings]
     result = ' '.join([' '.join(map(str, e)) for e in buildings])
     return result
+
+
+def get_hotspots(grid, mask, building, ncomponent, size):
+    r1, c1, r2, c2 = building
+    hotspots_grid = np.zeros_like(mask)
+
+    def _does_fit(row_, col_):
+        # extract possible hotspot
+        submatrix = mask[row_:row_ + size, col_:col_ + size]
+        if submatrix.shape[0] != 3 or submatrix.shape[1] != 3:
+            return False
+        # check if all cells are on the building
+        return np.all(submatrix == ncomponent)
+
+    for row in range(r1, r2 + 1):
+        for col in range(c1, c2 + 1):
+            if _does_fit(row, col):
+                hotspots_grid[row:row + size, col:col + size] = 1  # np.ones((size, size))
+
+    # plt.imshow(hotspots_grid)
+    # plt.show()
+
+    hotspots_mask, nhotspots = label(hotspots_grid)
+
+    # use the building algorithm again ...
+    hotspots = []
+    for nhotspots in range(1, nhotspots + 1):
+        hotspot = get_building(hotspots_grid, hotspots_mask, nhotspots)
+        hotspots.append(hotspot)
+
+    # get center points of hotspots
+    hotspots = [get_center_point(*a) for a in hotspots]
+
+    # hotspot center must be in on the building
+    hotspots = [e for e in hotspots if hotspots_grid[e[0], e[1]] == 1]
+
+    return hotspots
 
 
 def get_building(grid, mask, building_index):
@@ -52,11 +89,9 @@ def get_building(grid, mask, building_index):
     return r1, c1, r2, c2
 
 
-def is_hotspot(r1, c1, r2, c2):
-    min_row = 4
-    min_col = 4
-    return (r2 - r1) + 1 >= min_row \
-           and (c2 - c1) + 1 >= min_col
+def is_hotspot(size, r1, c1, r2, c2):
+    return (r2 - r1) + 1 >= size \
+           and (c2 - c1) + 1 >= size
 
 
 def get_center_point(r1, c1, r2, c2):
