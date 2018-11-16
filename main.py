024e44add5586c6bc1972348ee6bf334045d7b7d
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-11-16 17:00:09 +0100

    level4

--- main.py
+++ main.py
@@ -1,15 +1,68 @@
 import numpy as np
+from scipy.ndimage.measurements import label
 
 
 def main(input):
-    tuples = np.array(input["tuples"])
+    grid = np.array(input["rows"])
 
-    result = ''
-    for tuple in tuples:
-        c = paint_line(tuple, 0.001)
-        result += c + '\n'
+    mask, ncomponents = label(grid)
 
+    # is the floor in the upper left corner?
+    assert grid[0, 0] == 0
+
+    buildings = []
+    for ncomponent in range(1, ncomponents + 1):
+        building = get_building(grid, mask, ncomponent)
+
+        is_hot = is_hotspot(*building)
+        if is_hot:
+            buildings.append(building)
+
+    # get center points
+    buildings = [get_center_point(*a) for a in buildings]
+
+    # sort by row and by col
+    buildings = sorted(buildings, key=lambda x: (x[0], x[1]))
+
+    # prepend id and only output upper left corner
+    buildings = [(i, *a) for i, a in enumerate(buildings)]
+
+    print(buildings)
+    # [' '.join([' '.join(f) for f in e]) for e in buildings]
+    result = ' '.join([' '.join(map(str, e)) for e in buildings])
     return result
+
+
+def get_building(grid, mask, building_index):
+    r1, c1 = None, None
+    r2, c2 = None, None
+    for i, row in enumerate(mask):
+        if any(row == building_index):
+            fr = i
+            fc_start = np.argmax(row == building_index)
+            fc_end = len(row) - 1 - np.argmax(row[::-1] == building_index)
+
+            # set upper left corner point (first match)
+            if not r1 and not c1:
+                r1, c1 = fr, fc_start
+
+    # lower right corner point (last match)
+    r2, c2 = fr, fc_end
+
+    return r1, c1, r2, c2
+
+
+def is_hotspot(r1, c1, r2, c2):
+    min_row = 4
+    min_col = 4
+    return (r2 - r1) + 1 >= min_row \
+           and (c2 - c1) + 1 >= min_col
+
+
+def get_center_point(r1, c1, r2, c2):
+    rx = r1 + (r2 - r1) // 2
+    cx = c1 + (c2 - c1) // 2
+    return rx, cx
 
 
 def paint_line(tuple, stepsize):
