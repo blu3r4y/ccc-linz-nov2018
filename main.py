0b87c47fcb5ad9459b89598b3922b2dab4289229
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-11-16 15:37:02 +0100

    level2

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import numpy as np
 
 
 def main(input):
-    grid = np.array(input["rows"])
-    heights = sorted(np.unique(grid))
-    return ' '.join(map(str, heights[1:]))
+    tuples = np.array(input["tuples"])
+
+    result = ''
+    for tuple in tuples:
+        c = calculate(**tuple)
+        result += c + '\n'
+
+    return result
+
+
+def calculate(r1, c1, r2, c2, ratio):
+    a = np.array([r1 + 0.5, c1 + 0.5])
+    b = np.array([r2 + 0.5, c2 + 0.5])
+    t = b - a
+
+    c = np.floor(ratio * t + a)
+
+    return ' '.join(map(str, map(int, c)))
