25832c9a962858838b1bded4e5771fbddb57f530
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-11-16 15:48:13 +0100

    level3

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ def main(input):
 
     result = ''
     for tuple in tuples:
-        c = calculate(**tuple)
+        c = paint_line(tuple, 0.001)
         result += c + '\n'
 
     return result
 
 
-def calculate(r1, c1, r2, c2, ratio):
+def paint_line(tuple, stepsize):
+    cells = set()
+    for ratio in np.arange(0, 1, stepsize):
+        cell = intersection_cell(ratio, **tuple)
+        cells.add(cell)
+
+    return ' '.join(cells)
+
+
+def intersection_cell(ratio, r1, c1, r2, c2):
     a = np.array([r1 + 0.5, c1 + 0.5])
     b = np.array([r2 + 0.5, c2 + 0.5])
     t = b - a
